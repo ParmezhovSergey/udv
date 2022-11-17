979c1e8c7277cbdb48b68b2536159545ed54b096
ParmezhovSergey <parmezhovsergey@mail.ru>
2022-11-17 12:48:17 +0300

    deploy

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,14 +1,16 @@
 import styles from "../styles/Dialogues.module.css";
-import gamer from "../assets/icon/gamer.png";
+import hacker from "../assets/icon/hacker.png";
 import { IUser } from "../models/User";
 import { NavLink } from "react-router-dom";
 
 const UsersList: React.FC<IUser> = ({ name, id }) => {
   return (
     <NavLink to={`/message/${id}/${name}`} className={styles.user}>
-      <img className={styles.gamer} src={gamer} />
-
-      {name}
+      <img className={styles.gamer} src={hacker} />
+<div className={styles.name}>
+{name}
+</div>
+      
     </NavLink>
   );
 };
